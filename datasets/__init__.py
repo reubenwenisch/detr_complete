5f2aa632be8b858443d73b82a897593f37867b17
Reuben <wenisch@atom360.io>
2022-04-10 02:22:25 +0530

    Train model start

--- datasets/__init__.py
+++ datasets/__init__.py
@@ -3,7 +3,9 @@ import torch.utils.data
 import torchvision
 
 from .coco import build as build_coco
+from .oral import build as build_oral
 
+from .oral_panoptic import build as build_oral_panoptic
 
 def get_coco_api_from_dataset(dataset):
     for _ in range(10):
@@ -18,6 +20,11 @@ def get_coco_api_from_dataset(dataset):
 def build_dataset(image_set, args):
     if args.dataset_file == 'coco':
         return build_coco(image_set, args)
+    if args.dataset_file == 'oral':
+        return build_oral(image_set, args)
+    if args.dataset_file == 'oral_panoptic':
+        # to avoid making panopticapi required for coco
+        return build_oral_panoptic(image_set, args)
     if args.dataset_file == 'coco_panoptic':
         # to avoid making panopticapi required for coco
         from .coco_panoptic import build as build_coco_panoptic
