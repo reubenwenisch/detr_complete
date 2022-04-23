e631a2b265c0ce21a5fca89b40242de4b591ad79
Reuben-Turing-PC <reubenwenisch@gmail.com>
2022-04-23 14:10:18 +0530

    Panoptic box Training start error with bbox

--- util/box_ops.py
+++ util/box_ops.py
@@ -48,6 +48,8 @@ def generalized_box_iou(boxes1, boxes2):
     """
     # degenerate boxes gives inf / nan results
     # so do an early check
+    if (boxes2[:, 2:] <= boxes2[:, :2]).all():
+        print(boxes2)
     assert (boxes1[:, 2:] >= boxes1[:, :2]).all()
     assert (boxes2[:, 2:] >= boxes2[:, :2]).all()
     iou, union = box_iou(boxes1, boxes2)
